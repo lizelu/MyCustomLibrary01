48527c4a03733c7dfc9cc56efb0264c7807b2679
your_name <lizelusdut@qq.com>
2017-11-22 17:12:39 +0800

    Revert "Revert "Project init""
    This reverts commit a0c57a95cfb0384ba5b1e5c2d2799d126b7b0de5.

---
+++ MyCustomLibrary01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCustomLibrary01
+//
+//  Created by lizelu on 2017/11/22.
+//  Copyright © 2017年 lizelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
