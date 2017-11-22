5d91af00ec0a0e7e883c78c7f790534381555d7e
your_name <your_email>
2017-11-22 17:08:32 +0800

    Project init

---
+++ MyCustomLibrary01/Class/Hello.m
@@ -0,0 +1,16 @@
+//
+//  Hello.m
+//  MyCustomLibrary01
+//
+//  Created by lizelu on 2017/11/22.
+//  Copyright © 2017年 lizelu. All rights reserved.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+- (void)display
+{
+    NSLog(@"HELLO");
+}
+@end
---
+++ MyCustomLibrary01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCustomLibrary01
+//
+//  Created by lizelu on 2017/11/22.
+//  Copyright © 2017年 lizelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
